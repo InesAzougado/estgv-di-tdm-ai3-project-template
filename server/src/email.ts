e4f824552eed34fbd5f44579ea79886fb714a424
InesAzougado <57488512+InesAzougado@users.noreply.github.com>
2020-02-04 20:12:38 +0000

    send email

--- server/src/email.ts
+++ server/src/email.ts
@@ -6,10 +6,19 @@
  * @param message: string
 **/
 
-function sendEmail(
+export function sendEmail(
     from: string,
     to: string,
     subject: string,
     message: string){
-
+        const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+        to: to,
+        from: from,
+        subject: subject,
+        text: message,
+        html: '<strong>'+message+'</strong>',
+        };
+        return sgMail.send(msg);
     }
--- server/src/routes/Communication.ts
+++ server/src/routes/Communication.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 import { INTERNAL_SERVER_ERROR } from 'http-status-codes';
 import { buildApiErrorMessage } from '../shared/index';
+import { sendEmail } from '../email';
 
 const router = Router();
 
@@ -10,9 +11,20 @@ const router = Router();
  * @param res Response
  */
 async function handleSendEmail(req: Request, res: Response) {
-    // TODO: Handle sending of email via SendGrid
+    // TODO: Handle sending of email via SendGridconst SendEmailRequest = req.body as Api.SendEmailRequest;
+    const SendEmailRequest = req.body as Api.SendEmailRequest;
+    try{
+        sendEmail(
+            SendEmailRequest.email || 'inessousa898@gmail.com',
+            SendEmailRequest.destinatario,
+            SendEmailRequest.assunto,
+            SendEmailRequest.mensagem);
+        res.status(200);
+    } catch(err) {res.status(INTERNAL_SERVER_ERROR);}
+    
     res.status(INTERNAL_SERVER_ERROR)
         .send(buildApiErrorMessage('Not implemented'));
+
 }
 // Register routes
 router.post('/SendEmail', handleSendEmail);
